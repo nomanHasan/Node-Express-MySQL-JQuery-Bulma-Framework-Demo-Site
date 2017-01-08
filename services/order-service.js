98e166bd1224e2c2833270507b85acf04784796a
nomanHasan <nomanbinhussein@gmail.com>
2017-01-08 16:45:02 +0600

    Second COmmit

---
+++ services/order-service.js
@@ -0,0 +1,47 @@
+var db = require('../config/db');
+
+
+
+exports.demo = function () {
+
+    db.connect(function (err) {
+        if (err) {
+            console.log('Connection Error');
+            return;
+        }
+        db.query('SELECT * FROM `food`', function (err, result) {
+            console.log(result);
+        });
+    });
+}
+
+exports.getOrders = function (next) {
+    db.query('SELECT * FROM `order`', function (err, result) {
+        console.log(result);
+        return next(result);
+    });
+}
+
+exports.getFood = function (name, next) {
+    db.query('SELECT * FROM `food` where name =\''+name+'\'', function (err, result) {
+        console.log(result);
+        return next(result[0]);
+    });
+}
+
+exports.updateFood = function (name, food, next) {
+    var queryString = 'update `food` SET name=\''+food.name+'\', type=\''+food.type+'\', quantity='+food.quantity+', price='+food.price+' where name =\''+name+'\'';
+    console.log(queryString);
+    db.query(queryString, function (err, result) {
+        console.log(result);
+        return next(result[0]);
+    });
+}
+
+exports.addFood = function (food, next) {
+    db.query('insert into `food` (name, type, quantity, price) VALUES(\''+food.name+'\', \''+food.type+'\', '+food.quantity+', '+food.price+')', function (err, result) {
+        console.log(result);
+        return next(result[0]);
+    });
+}
+
